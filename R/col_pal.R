fdfba345b9f4ac6fed824771862a87b3340109f6
Close-your-eyes <vonskopnik@pm.me>
2025-11-26 10:21:28 +0100

    up

--- R/col_pal.R
+++ R/col_pal.R
@@ -29,8 +29,8 @@ col_pal <- function(name = NULL,
 
   paletteers <-
     dplyr::bind_rows(dplyr::mutate(paletteer::palettes_c_names, type2 = "continuous"),
-                     dplyr::mutate(paletteer::palettes_d_names, type2 = "discrete"))
-  paletteers <- dplyr::mutate(paletteers, command = paste0(package, "::", palette))
+                     dplyr::mutate(paletteer::palettes_d_names, type2 = "discrete")) |>
+    dplyr::mutate(command = paste0(package, "::", palette))
 
   if (is.null(name)) {
     message("Select one palette by palette or command. Additional ones are 'custom', 'ggplot', 'hue', 'material'.")
@@ -44,91 +44,78 @@ col_pal <- function(name = NULL,
     if (is.null(n) || n == 0) {
       n <- 100
     }
-    pal_return <- prismatic::color(scales::hue_pal()(n))
-    if (direction == -1) {
-      pal_return <- rev(pal_return)
-    }
-  } else if (name == "custom") {
-    pal_return <- prismatic::color(c("grey65", "darkgoldenrod1", "cornflowerblue", "forestgreen", "tomato2", "mediumpurple1", "turquoise3", "lightgreen", "navy", "plum1",
-                                     "red4", "khaki1", "tan4", "cadetblue1", "olivedrab3", "darkorange2", "burlywood2", "violetred3", "aquamarine3",
-                                     "grey30", "lavender", "blueviolet", "grey10", "pink3", "turquoise4", "darkkhaki", "magenta", "blue", "green", "red",
-                                     "darkolivegreen", "orchid1", "springgreen", "dodgerblue4", "deepskyblue", "palevioletred4", "gold4", "maroon1",
-                                     "lightyellow", "greenyellow", "purple4", "yellow"))
-    if (!is.null(n)) {
-      if (n > length(pal_return)) {
-        pal_return <- prismatic::color(scales::hue_pal()(n))
-      }
-      pal_return <- pal_return[1:n]
-    }
-    if (direction == -1) {
-      pal_return <- rev(pal_return)
-    }
-  } else if (name == "material") {
-    # bg_col: '#273238'
-    pal_return <- prismatic::color(c("#CC6666", "#DF935F", "#81A3BE","#B5BD68", "#707880", "#B394BB", "#4F5C69", "#CC0000", "#F1CC37",
-                                     "#5787DA", "#25B876", "#5E3582", "#93C5DE", "#B7A7C5", "#2E90B0", "#C5C8C6", "#50C186")) # "#34312F"
+    pal_return <- scales::hue_pal()(n)
 
-    if (!is.null(n)) {
+  } else if (name %in% c("custom", "material", "custom_light")) {
+
+    pal_return <- utils::getFromNamespace(name, "colrr")
+    if (!is.null(n) && n > 0) {
       if (n > length(pal_return)) {
-        pal_return <- prismatic::color(scales::hue_pal()(n))
+        pal_return <- scales::hue_pal()(n)
       }
       pal_return <- pal_return[1:n]
     }
-    if (direction == -1) {
-      pal_return <- rev(pal_return)
-    }
+
   } else {
+
     pal_select <- ifelse(grepl("::", name),
                          list(dplyr::filter(paletteers, tolower(command) == tolower(name))),
                          list(dplyr::filter(paletteers, tolower(palette) == tolower(name))))[[1]]
 
-  if (nrow(pal_select) == 0) {
-    stop("Palette not found.")
-  } else if (nrow(pal_select) > 1) {
-    # special cases: prefer colorbrewer or viridis pkg by default
-    if (nrow(pal_select) == 2 && sum(grepl("RColorBrewer", pal_select$package)) == 1) {
-      pal_select <- dplyr::filter(pal_select, package == "RColorBrewer")
-    } else if (nrow(pal_select) > 1 && sum(grepl("viridis", pal_select$package)) == 1) {
-      pal_select <- dplyr::filter(pal_select, package == "viridis")
-    } else  {
-      print(pal_select)
-      stop("Name is ambiguous. Please specify by command.")
+    if (nrow(pal_select) == 0) {
+      stop("Palette not found.")
+    } else if (nrow(pal_select) > 1) {
+      # special cases: prefer colorbrewer or viridis pkg by default
+      if (nrow(pal_select) == 2 && sum(grepl("RColorBrewer", pal_select$package)) == 1) {
+        pal_select <- dplyr::filter(pal_select, package == "RColorBrewer")
+      } else if (nrow(pal_select) > 1 && sum(grepl("viridis", pal_select$package)) == 1) {
+        pal_select <- dplyr::filter(pal_select, package == "viridis")
+      } else  {
+        print(pal_select)
+        stop("Name is ambiguous. Please specify by command.")
+      }
+    }
+
+    if (pal_select$type2 == "discrete") {
+      type <- "discrete"
+      if (is.null(n) || n == 0) {
+        n <- pal_select$length
+      }
+      if (n > pal_select$length) {
+        #message("n = ", n, " larger than number of discrete color in palette (", pal_select$length, "). Going to interpolate to provide ", n, " colors.")
+        type <- "continuous"
+      }
+      # what if n>pal_select$length - fall back to hue pal?
+      pal_return <- paletteer::paletteer_d(pal_select$command, n = n, type = type, direction = 1)
+
+    } else if (pal_select$type2 == "continuous") {
+      if (is.null(n) || n == 0) {
+        n <- 100
+      }
+      pal_return <- paletteer::paletteer_c(pal_select$command, n = n, direction = 1)
     }
   }
 
-  if (pal_select$type2 == "discrete") {
-    type <- "discrete"
-    if (is.null(n) || n == 0) {
-      n <- pal_select$length
-    }
-    if (n > pal_select$length) {
-      #message("n = ", n, " larger than number of discrete color in palette (", pal_select$length, "). Going to interpolate to provide ", n, " colors.")
-      type <- "continuous"
-    }
-    # what if n>pal_select$length - fall back to hue pal?
-    pal_return <- paletteer::paletteer_d(pal_select$command, n = n, type = type, direction = direction)
 
-  } else if (pal_select$type2 == "continuous") {
-    if (is.null(n) || n == 0) {
-      n <- 100
-    }
-    pal_return <- paletteer::paletteer_c(pal_select$command, n = n, direction = direction)
+
+  if (contrast_filter) {
+    ratios <- purrr::map_dbl(stats::setNames(pal_return, pal_return), contrast_ratio, hex2 = bg_color)
+    ratios <- ratios[which(ratios > contrast_ratio_min)]
+    pal_return <- names(ratios)
   }
-}
 
-if (contrast_filter) {
+  if (direction == -1) {
+    pal_return <- rev(pal_return)
+  }
+  pal_return <- prismatic::color(pal_return)
 
-  ratios <- purrr::map_dbl(stats::setNames(pal_return, pal_return), contrast_ratio, hex2 = bg_color)
-  ratios <- ratios[which(ratios > contrast_ratio_min)]
-  pal_return <- prismatic::color(names(ratios))
-}
-
-if (length(pal_return) > 200) {
-  invisible(pal_return)
-} else {
-  return(pal_return)
-}
+  if (length(pal_return) > 200) {
+    invisible(pal_return)
+  } else {
+    return(pal_return)
+  }
 
 }
+
 
 if(base::getRversion() >= "2.15.1")  utils::globalVariables(c("package", "palette", "command"))
